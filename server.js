fa18c899fe17e81916932237158ef04edae64e68
Lucas <lukefranc3@yahoo.com>
2023-09-26 20:18:54 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.static('public'));
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+
+    const transportToDev = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'lukefranc3@gmail.com',
+            pass: 'paoottpbcnusbqqk'
+        }
+    })
+
+    // const transportToClient = nodemailer.createTransport({
+    //     service: 'gmail',
+    //     auth: {
+    //         user: '***MARKS EMAIL***',
+    //         pass: '***MARKS PASSWORD***'
+    //     }
+    // })
+
+    const mailOptionsDev = {
+        from: req.body.email,
+        to: 'lukefranc3@gmail.com',
+        subject: `MK Drone and Media: New Message from ${req.body.email}`,
+        text: `
+        First name: ${req.body.firstName}
+        Last name: ${req.body.lastName}
+        Email: ${req.body.email}
+        Phone number: ${req.body.number}
+
+        Message: 
+        ${req.body.message}
+        `
+    }
+
+    // const mailOptionsClient = {
+    //     from: req.body.email,
+    //     to: '***MARKS EMAIL***',
+    //     subject: `MK Drone and Media: New Message from ${req.body.email}`,
+    //     text: `
+    //     First name: ${req.body.firstName}
+    //     Last name: ${req.body.lastName}
+    //     Email: ${req.body.email}
+    //     Phone number: ${req.body.number}
+
+    //     Message: 
+    //     ${req.body.message}
+    //     `
+    // }
+
+    transportToDev.sendMail(mailOptionsDev, (error, info) => {
+        if(error){
+            console.log(error);
+            res.send(error);
+        } else{
+            console.log('Email sent:' + info.response);
+            res.send('success');
+        }
+    });
+
+    // transportToClient.sendMail(mailOptionsClient, (error, info) => {
+    //     if(error){
+    //         console.log(error);
+    //         res.send(error);
+    //     } else{
+    //         console.log('Email sent:' + info.response);
+    //         res.send('success');
+    //     }
+    // });
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
