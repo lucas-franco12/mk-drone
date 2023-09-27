0c4512ce4728b144a0120c7bd72021e8534b4feb
Lucas <lukefranc3@yahoo.com>
2023-09-27 13:01:58 -0400

    Updated CORS middleware

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.json())
 
 // CORS middleware 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'https://main--mkdroneandmedia.netlify.app'); // Replace '*' with specific allowed origins
+    res.header('Access-Control-Allow-Origin', 'https://mkdroneandmedia.netlify.app'); // Replace '*' with specific allowed origins
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
